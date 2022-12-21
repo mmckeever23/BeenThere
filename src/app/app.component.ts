5c4bfa3bf673ac56bef9076007e9d3b00cfb1ee8
Matt McKeever <mmckeever23@gmail.com>
2022-12-20 20:07:27 -0600

    Updated map properties, added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit{
   title = 'BeenThere';
 
@@ -15,10 +17,58 @@ export class AppComponent implements OnInit{
     })
 
     loader.load().then(() => {
-      new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      let map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 0, lng: 0 },
         zoom: 2,
+        streetViewControl: false,
+        fullscreenControl: false,
+      });
+
+      let marker = new google.maps.Marker({
+        position: {lat: 38.6272700, lng: -90.1978900},
+        map: map,
+        icon: "https://img.icons8.com/tiny-color/32/null/map-pin.png",
+        draggable: true
+      }); 
+      
+        let content = 
+        "<h2>Create New Pin</h2>" +
+          "<form>" +
+            "<label>Location: </label>" +
+            "<input type='text' required><br>" +
+            "<br>" +
+            "<label>Date Arrived: </label>" +
+            "<input type='date' required><br>" +
+            "<br>" +
+            "<label>Date Departed: </label>" +
+            "<input type='date' required><br>" +
+            "<br>" +
+            "<label>Companions: </label><br>" +
+            "<input type='checkbox'>" +
+            "<label>Mom</label><br>" +
+            "<input type='checkbox'>" +
+            "<label>Dad</label><br>" +
+            "<input type='checkbox'>" +
+            "<label>Julie</label><br>" +
+            "<p style='margin-left: 13px'>+ Add Companion</p>" +
+            "<label>Details: </label><br>" +
+            "<textarea placeholder='What did you do there?' rows='10' cols='29'></textarea><br>" +
+            "<br>" +
+            "<input type='submit' value='Create Pin'>" +
+            "<br>" +
+          "</form>"
+
+      let infoWindow = new google.maps.InfoWindow({
+        content: content,
+      })
+
+      marker.addListener("click", function() {
+        infoWindow.open({
+          anchor: marker,
+          map,
+        });
       });
     });
-  }
-}
+  };
+};
+
