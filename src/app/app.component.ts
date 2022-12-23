f2341155d4affe8d81ba0c731bb52db7ca103be7
Matt McKeever <mmckeever23@gmail.com>
2022-12-23 17:00:54 -0600

    Drop multiple pins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,6 +30,7 @@ export class AppComponent implements OnInit{
     loader.load().then(() => {
       
 //Load map
+
       map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 0, lng: 0 },
         zoom: 2,
@@ -56,17 +57,7 @@ export class AppComponent implements OnInit{
   searchBox.addListener("places_changed", () => {
     const places: any = searchBox.getPlaces();
 
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
+// For each place, get the icon, name and location.
     const bounds = new google.maps.LatLngBounds();
 
     places.forEach((place: any) => {
@@ -83,18 +74,26 @@ export class AppComponent implements OnInit{
         scaledSize: new google.maps.Size(25, 25),
       };
 
-      // Create a marker for each place.
-      markers.push(
-        new google.maps.Marker({
+// Create a marker for each place.
+
+      const infowindow: any = new google.maps.InfoWindow({
+        content: form,
+      });
+
+      const marker: any = new google.maps.Marker({
           map,
-          icon,
+          icon: "https://img.icons8.com/tiny-color/32/null/map-pin.png",
           title: place.name,
           position: place.geometry.location,
-        })
-      );
+          draggable: true
+          })
+          infowindow.open({
+            anchor: marker,
+            map,
+          })
 
       if (place.geometry.viewport) {
-        // Only geocodes have viewport.
+// Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
       } else {
         bounds.extend(place.geometry.location);
@@ -105,59 +104,57 @@ export class AppComponent implements OnInit{
 
 //Event listener for dropping pin
 
-      map.addListener("click", (event: google.maps.MapMouseEvent) => {
-        addMarker(event.latLng!);
-        });
+      // map.addListener("click", (event: google.maps.MapMouseEvent) => {
+      //   addMarker(event.latLng!);
+      //   });
 
-//Function for dropping pin
-
-      function addMarker(position: google.maps.LatLng | google.maps.LatLngLiteral) {
-        const marker = new google.maps.Marker({
-          position,
-          map,
-          icon: "https://img.icons8.com/tiny-color/32/null/map-pin.png",
-          draggable: true
-        })
-        infoWindow.open({
-          anchor: marker,
-          map,
-        }) 
-      }
-
-//New Pin form
+        //New Pin form
 
       let form = 
-        "<h2>Create New Pin</h2>" +
-          "<form>" +
-            "<label>Location: </label>" +
-            "<input type='text' required><br>" +
-            "<br>" +
-            "<label>Date Arrived: </label>" +
-            "<input type='date' required><br>" +
-            "<br>" +
-            "<label>Date Departed: </label>" +
-            "<input type='date' required><br>" +
-            "<br>" +
-            "<label>Companions: </label><br>" +
-            "<input type='checkbox'>" +
-            "<label>Mom</label><br>" +
-            "<input type='checkbox'>" +
-            "<label>Dad</label><br>" +
-            "<input type='checkbox'>" +
-            "<label>Julie</label><br>" +
-            "<p style='margin-left: 13px'>+ Add Companion</p>" +
-            "<label>Details: </label><br>" +
-            "<textarea placeholder='What did you do there?' rows='10' cols='29'></textarea><br>" +
-            "<br>" +
-            "<input type='submit' value='Create Pin'>" +
-            "<br>" +
-          "</form>"
+      "<h2>Create New Pin</h2>" +
+        "<form>" +
+          "<label>Location: </label>" +
+          "<input type='text' required><br>" +
+          "<br>" +
+          "<label>Date Arrived: </label>" +
+          "<input type='date' required><br>" +
+          "<br>" +
+          "<label>Date Departed: </label>" +
+          "<input type='date' required><br>" +
+          "<br>" +
+          "<label>Companions: </label><br>" +
+          "<input type='checkbox'>" +
+          "<label>Mom</label><br>" +
+          "<input type='checkbox'>" +
+          "<label>Dad</label><br>" +
+          "<input type='checkbox'>" +
+          "<label>Julie</label><br>" +
+          "<p style='margin-left: 13px'>+ Add Companion</p>" +
+          "<label>Details: </label><br>" +
+          "<textarea placeholder='What did you do there?' rows='10' cols='29'></textarea><br>" +
+          "<br>" +
+          "<input type='submit' value='Create Pin'>" +
+          "<br>" +
+        "</form>"
 
 //Info Window with New Pin form
 
-      const infoWindow = new google.maps.InfoWindow({
-        content: form, 
-      })
+
+
+// //Function for dropping pin
+
+      // function addMarker(position: google.maps.LatLng | google.maps.LatLngLiteral) {
+      //   const marker = new google.maps.Marker({
+      //     position,
+      //     map,
+      //     icon: "https://img.icons8.com/tiny-color/32/null/map-pin.png",
+      //     draggable: true
+      //   })
+      //   infoWindow.open({
+      //     anchor: marker,
+      //     map,
+      //   }) 
+      // }
     })
   }
 }   
