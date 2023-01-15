eb3722970a2e84a6e2777e9e59172f6c1d6e87bd
Matt McKeever <mmckeever23@gmail.com>
2023-01-14 23:24:50 -0600

    Need to convert from object to two numbers

---
+++ src/app/pin-data/pin-data.component.spec.ts
@@ -0,0 +1,23 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { PinDataComponent } from './pin-data.component';
+
+// describe('PinDataComponent', () => {
+//   let component: PinDataComponent;
+//   let fixture: ComponentFixture<PinDataComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ PinDataComponent ]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(PinDataComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
