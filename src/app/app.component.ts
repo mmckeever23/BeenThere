69bed65b3f2e127a2130fe3e576bff67e89714a2
Matt McKeever <mmckeever23@gmail.com>
2023-01-17 12:13:36 -0600

    Saved pins appearing on map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, platformCore } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PinModalComponent } from './pin-modal/pin-modal.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { Pin } from './pin';
@@ -19,11 +19,14 @@ export class AppComponent implements OnInit{
   title = 'frontend';
   data = '';
   pin: Pin = new Pin();
+  pins: Pin[]=[];
   
   constructor(private modalService: NgbModal, private pinDataService: PindataService) {}
 
-  coordinatesSave(){
-    this.pinDataService.saveCoordinates(this.pin).subscribe({
+//Function for saving pins
+
+  pinSave(){
+    this.pinDataService.savePins(this.pin).subscribe({
       next: (data) => {},
       error: (error) => {
         alert("There was a problem creating the pin.");
@@ -60,10 +63,9 @@ export class AppComponent implements OnInit{
      
 // Render Login modal
 
-      // this.openLoginModal();
+      this.openLoginModal();
 
 // Render map
-
       map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 35, lng: 5 },
         zoom: 3,
@@ -81,6 +83,15 @@ export class AppComponent implements OnInit{
         }
       });
 
+//Render saved pins
+
+      for (let i = 0; i<this.pins.length; i++) {
+        new google.maps.Marker({
+          position: { lat: Number(this.pins[i].lat), lng: Number(this.pins[i].lng) },
+          map,
+        })
+      }
+
 // Create the search box and link it to the UI element
 
       const input = document.getElementById("pac-input") as HTMLInputElement;
@@ -96,7 +107,7 @@ export class AppComponent implements OnInit{
         places.forEach((place: any) => {
 
 // Create a marker
-
+          
           const marker = new google.maps.Marker({
             map,
             icon: "https://img.icons8.com/tiny-color/32/null/map-pin.png",
@@ -105,21 +116,30 @@ export class AppComponent implements OnInit{
             animation: google.maps.Animation.DROP,
             })
           this.data=place.name;
+          this.pin.name=place.name;
           map.setCenter(marker.getPosition() as google.maps.LatLng);
           setTimeout(()=>{this.openPinModal()}, 1000);
           this.pin.lat = String(marker.getPosition()!.lat());
           this.pin.lng = String(marker.getPosition()!.lng());
-          this.coordinatesSave();
-                    
+          this.pinSave();
+                                     
 // Click handler for pre-existing marker
 
           marker.addListener("click", () => {
             this.data=place.name;
             map.setCenter(marker.getPosition() as google.maps.LatLng);
             this.openPinModal();
-            })
+            })  
         })
       })
+
+    })
+
+//
+
+    this.pinDataService.getAllPins().subscribe(data=>{
+      this.pins=data;
+
     })
   }
 }   
