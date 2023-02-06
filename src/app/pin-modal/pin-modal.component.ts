e33bc7120f6e3e7edd63b59a9e0bc432631d7272
Matt McKeever <mmckeever23@gmail.com>
2023-02-05 21:33:11 -0600

    User to Pin Relationship Working!

--- src/app/pin-modal/pin-modal.component.ts
+++ src/app/pin-modal/pin-modal.component.ts
@@ -7,7 +7,6 @@ import { PindataService } from '../pindata.service';
   templateUrl: './pin-modal.component.html',
   styleUrls: ['./pin-modal.component.css']
 })
-
 export class PinModalComponent {
 
   @Input() data: any;
@@ -16,23 +15,23 @@ export class PinModalComponent {
   @Input() pins: any;
 
   constructor(private pinDataService: PindataService, public activeModalService: NgbActiveModal){}
-  
+
   ngOnInit(): void {
     this.pinDataService.getPinById(this.pin.id).subscribe({
       next: (data) => {
         this.pin = data;
-      }
+      }, 
     })
   }
 
   onSubmit(){
     this.pinDataService.savePin(this.pin).subscribe({
       next: (data) => {
-        alert("Pin created! Press 'Save Updates' to save the pin.");
+        alert("Pin saved!");
         this.activeModalService.dismiss();
       },
       error: (error) => {
-        alert("There was a problem creating this pin.");
+        alert("There was a problem saving this pin.");
       }
     }) 
   }
